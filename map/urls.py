9c553c78373fd1af26a2f035ce133114f40726f2
suhailvs <suhailvs@gmail.com>
2023-12-27 05:38:50 +0530

    removed property app

--- map/urls.py
+++ map/urls.py
@@ -7,4 +7,5 @@ from . import views
 app_name = "map"
 urlpatterns = [
     path("save_property/", views.save_property, name="save_property"),
+    path('<int:property>/', views.PropertyDetail.as_view(), name='property_detail'),
 ]
